41597454b349e82e8e73f3c905fe3129d1558d7c
omidkhalafbeigi <omidkhalafbeigi903@gmail.com>
2020-05-22 11:25:15 +0430

    Problem4_B

---
+++ Problem4_B.py
@@ -0,0 +1,7 @@
+import Problem4_A as P4_A
+import time
+
+# x[n] = Delta[n] -> [LTI System] -> y[n] = h[n]
+# pay attention: factors must be the float type (like (0.2) instead of (1/5))
+
+P4_A.recursion_eq(1, 0)
